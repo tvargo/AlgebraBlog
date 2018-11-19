bc43232161926f74b31b488c1edb58d866c63fa1
programer10 <programer10@ucione.local>
2018-11-19 18:06:36 +0100

    migracija

---
+++ database/migrations/2018_11_13_194235_change_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('slug', 191)->AFTER('title');
+			$table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('slug');
+			$table->dropForeign(['user_id']);
+        });
+    }
+}
